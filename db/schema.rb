484f19e768d45db75fd5bb1b0205f0ce02563345
brandoncha <brandoncha2005@gmail.com>
2016-03-13 00:21:55 +0000

    Setting up image uploading

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160311065808) do
+ActiveRecord::Schema.define(version: 20160312225420) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 20160311065808) do
     t.integer  "user_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "image"
   end
 
   add_index "courses", ["user_id"], name: "index_courses_on_user_id", using: :btree
@@ -59,6 +60,7 @@ ActiveRecord::Schema.define(version: 20160311065808) do
     t.inet     "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "image"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
