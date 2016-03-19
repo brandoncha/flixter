bd438fa8a4f9a02a0a327ec2a9b20ec9dd3b7a07
brandoncha <brandoncha2005@gmail.com>
2016-03-19 07:02:50 +0000

    Styling the Instructor Course Overview Page

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -25,7 +25,7 @@ class ImageUploader < CarrierWave::Uploader::Base
   # end
 
   # Process files as they are uploaded:
-   process resize_to_fill: [800, 550]
+   process resize_to_fill: [1000, 550]
   #
   # def scale(width, height)
   #   # do something
